c5880ba7ada136ac3b89a4f262da6e640c0f1863
himynameisruss <himynameisruss@gmail.com>
2016-11-17 22:10:24 -0500

    Top menu

--- Champs.js
+++ Champs.js
@@ -1,17 +1,39 @@
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
+document.addEventListener("mouseover", function(event){
+
+  if (event.target.matches('.navigation-item-title')) {
+
+	var parent = event.target.parentElement;
+    var dropdowns = parent.getElementsByClassName('navigation-popup-items')[0]
+	if (dropdowns != null){
+  		dropdowns.classList.remove('hide');
+	}
+  } else {
+	  var parent = event.target.parentNode;
+	  if (parent != null){
+		  var classList = parent.classList;
+		if (classList != null && classList.contains('navigation-popup-items')){
+	  		parent.classList.remove('hide');
+		}	
+	  }
   }
+  
+});
+
+document.addEventListener("mouseout", function(event){
+		hideAllPopups();
+});
+
+function hideAllPopups(){
+	var items = document.getElementsByClassName('navigation-popup-items');
+	if (items != null){
+		for (var i = 0; i < items.length; i++){
+			var item = items[i];
+	  		item.classList.add('hide');
+		}
+	}
 }
+
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
