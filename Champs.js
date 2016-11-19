81f32a3a8bccffa6d67619d860b1ccd9f793830b
himynameisruss <himynameisruss@gmail.com>
2016-11-18 19:16:39 -0500

    Menu stuff

--- Champs.js
+++ Champs.js
@@ -1,3 +1,30 @@
+$(function() {
+    // Stick the #nav to the top of the window
+    var nav = $('.navigation');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
 
 document.addEventListener("mouseover", function(event){
 
